8f2196113b20f5fa4ba7b092f810c7b648704a95
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-12-20 11:57:37 -0600

    Refactor PostItem component to enhance layout with CSS modules, adding structured content elements for improved presentation.

--- components/posts/post-item.tsx
+++ components/posts/post-item.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import Image from "next/image";
+import classes from "./post-item.module.css";
 
 export default function PostItem( props ) {
   return (
-    <li>
+    <li className={classes.post}>
       <Link href={`/posts/${props.post.id}`}>
         <a>
-          <Image src={props.post.image} alt={props.post.title} width={300} height={200} />
+          <div className={classes.image}>
+            <Image src={props.post.image} alt={props.post.title} width={300} height={200} />
+          </div>
+          <div className={classes.content}>
+            <h3>{props.post.title}</h3>
+            <time>{props.post.date}</time>
+            <p>{props.post.excerpt}</p>
+          </div>
         </a>
       </Link>
     </li>
