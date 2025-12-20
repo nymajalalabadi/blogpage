ead6587268d20a4f36a19b3c98b22acb3ae759ec
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-12-20 12:56:24 -0600

    Add AllPosts component to Posts page and implement dummy posts data for display.

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,8 +1,38 @@
+import AllPosts from "@/components/posts/All-Posts";
 
-export default function Posts() {
+const DUMMY_POSTS = [
+  {
+    title: "Getting Started with Next.js",
+    image: "getting-started-nextjs.png",
+    excerpt: "Learn how to build a personal website using Next.js",
+    date: "2025-01-01",
+    slug: "getting-started-with-nextjs",
+  },
+  {
+    title: "Getting Started with React",
+    image: "getting-started-nextjs.png",
+    excerpt: "Learn how to build a personal website using React",
+    date: "2025-01-01",
+    slug: "getting-started-with-nextjs2",
+  },
+  {
+    title: "Getting Started with Angular",
+    image: "getting-started-nextjs.png",
+    excerpt: "Learn how to build a personal website using Angular",
+    date: "2025-01-01",
+    slug: "getting-started-with-nextjs3",
+  },
+  {
+    title: "Getting Started with Vue",
+    image: "getting-started-nextjs.png",
+    excerpt: "Learn how to build a personal website using Vue",
+    date: "2025-01-01",
+    slug: "getting-started-with-nextjs4",
+  },
+];
+
+export default function AllPostsPage() {
   return (
-    <div>
-      <h1>Posts</h1>
-    </div>
+      <AllPosts posts={DUMMY_POSTS} />
   );
 }
