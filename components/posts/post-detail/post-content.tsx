f687eb2250b82b3de25b3c5c0e803ee7fb467047
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-12-21 10:45:15 -0600

    Integrate react-syntax-highlighter for enhanced code block rendering in PostContent. Update package dependencies and styles for improved markdown presentation. Modify featured property in markdown file for consistency.

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -1,6 +1,8 @@
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 interface PostContentProps {
   title: string;
@@ -15,7 +17,30 @@ export default function PostContent({ title, image, content, slug }: PostContent
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          code(props: any) {
+            const { inline, className, children } = props;
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={oneDark}
+                language={match[1]}
+                PreTag="div"
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </article>
   );
 }
