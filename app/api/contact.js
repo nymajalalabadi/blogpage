a4a68e8f7b29923e42d7c3bb76b601ebaf2d21ef
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-12-22 10:42:42 -0600

    Add MongoDB integration to contact API for message storage

--- app/api/contact.js
+++ app/api/contact.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { email, name, message } = req.body;
 
@@ -13,9 +15,29 @@ export default function handler(req, res) {
             name,
             message
         }
-        console.log(newMessage);
 
-        res.status(200).json({ message: 'Message sent successfully' });
+        let client;
+
+        try {
+            client = await MongoClient.connect('mongodb+srv://yukari:81818181@cluster0.lz9vx.mongodb.net/?retryWrites=true&w=majority&appName=cluster0');
+        } catch (error) {
+            res.status(500).json({ message: 'Storing message failed.' });
+        }
+
+        const db = client.db('blog');
+
+        try {
+            const result = await db.collection('messages').insertOne(newMessage);
+            newMessage.id = result.insertedId;
+        } catch (error) {
+            client.close();
+            res.status(500).json({ message: 'Storing message failed.' });
+            return;
+        } 
+
+        client.close();
+
+        res.status(200).json({ message: 'Message sent successfully', message: newMessage });
     } else {
         res.status(405).json({ message: 'Method not allowed' });
     }
